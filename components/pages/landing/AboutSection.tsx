c2c8f8ed57347798a94beaa50b38fa51876ff13d
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2025-07-13 11:33:45 -0600

    landing page animations

--- components/pages/landing/AboutSection.tsx
+++ components/pages/landing/AboutSection.tsx
@@ -1,26 +1,81 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { FaArrowRight } from "react-icons/fa";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { easeOut } from "framer-motion";
+
+const fadeInUp = (delay = 0) => ({
+	hidden: { opacity: 0, y: 30 },
+	visible: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			duration: 0.4,
+			delay,
+			ease: easeOut,
+		},
+	},
+});
 
 const AboutSection = () => {
+	const controls1 = useAnimation();
+	const controls2 = useAnimation();
+	const controls3 = useAnimation();
+
+	const [ref1, inView1] = useInView({ triggerOnce: true });
+	const [ref2, inView2] = useInView({ triggerOnce: true });
+	const [ref3, inView3] = useInView({ triggerOnce: true });
+
+	useEffect(() => {
+		if (inView1) controls1.start("visible");
+		if (inView2) controls2.start("visible");
+		if (inView3) controls3.start("visible");
+	}, [inView1, inView2, inView3, controls1, controls2, controls3]);
+
 	return (
 		<div className="">
 			<div className="h-[105vh] flex flex-col gap-[3vh] items-center justify-center">
-				<div className="w-[85vw] border-2 border-black rounded-[10vh] flex min-h-[40vh] px-[3vw] py-[3vh]">
+				{/* Card 1 */}
+				<motion.div
+					ref={ref1}
+					initial="hidden"
+					animate={controls1}
+					variants={fadeInUp(0)}
+					className="w-[85vw] border-2 border-black rounded-[10vh] flex min-h-[40vh] px-[3vw] py-[3vh]"
+				>
 					<div className="w-3/5 flex flex-col pl-[3vw] items-start justify-center pr-[3vw]">
-						<div className="flex items-center justify-start gap-[2vw]">
+						<motion.div
+							initial="hidden"
+							animate={controls1}
+							variants={fadeInUp(0.3)}
+							className="flex items-center justify-start gap-[2vw]"
+						>
 							<span className="maestro text-[4vh]">Mediblob</span>
 							<img
 								src="/stars/star5.png"
 								className="h-[7vh] aspect-[1]"
 								alt=""
 							/>
-						</div>
-						<p className="text-[3vh] font-extralight pt-[1vh] pb-[2.5vh]">
+						</motion.div>
+
+						<motion.p
+							initial="hidden"
+							animate={controls1}
+							variants={fadeInUp(0.5)}
+							className="text-[3vh] font-extralight pt-[1vh] pb-[2.5vh]"
+						>
 							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-						</p>
-						<div className="flex items-center justify-center w-full gap-[1vw]">
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</motion.p>
+
+						<motion.div
+							initial="hidden"
+							animate={controls1}
+							variants={fadeInUp(0.7)}
+							className="flex items-center justify-center w-full gap-[1vw]"
+						>
 							<Link
 								href={"/"}
 								className="flex items-center justify-center rounded-[2.75vh] w-full py-[1.5vh] font-bold border-2 border-black bg-neutral-800 text-white text-[2.25vh]"
@@ -36,21 +91,45 @@ const AboutSection = () => {
 									<FaArrowRight />
 								</span>
 							</Link>
-						</div>
+						</motion.div>
 					</div>
-					<div className="w-2/5 ">
+
+					<motion.div
+						initial="hidden"
+						animate={controls1}
+						variants={fadeInUp(0.9)}
+						className="w-2/5"
+					>
 						<img
 							src="/banner.jpg"
 							className="w-full h-full object-cover rounded-[10vh] border-2 border-black"
 							alt=""
 						/>
-					</div>
-				</div>
-				<div className="w-[85vw] border-2 border-black rounded-[10vh] flex items-center justify-between min-h-[15vh] px-[3vw] py-[2vh]">
-					<div className="text-[3vh] pl-[1vw]">
+					</motion.div>
+				</motion.div>
+
+				{/* Card 2 */}
+				<motion.div
+					ref={ref2}
+					initial="hidden"
+					animate={controls2}
+					variants={fadeInUp(0.3)}
+					className="w-[85vw] border-2 border-black rounded-[10vh] flex items-center justify-between min-h-[15vh] px-[3vw] py-[2vh]"
+				>
+					<motion.div
+						initial="hidden"
+						animate={controls2}
+						variants={fadeInUp(0.5)}
+						className="text-[3vh] pl-[1vw]"
+					>
 						Lorem ipsum dolor sit amet consectetur adipisicing.
-					</div>
-					<div className="flex gap-[1vw]">
+					</motion.div>
+					<motion.div
+						initial="hidden"
+						animate={controls2}
+						variants={fadeInUp(0.7)}
+						className="flex gap-[1vw]"
+					>
 						<img
 							src="/banner.jpg"
 							className="w-[15vw] h-[13vh] object-cover rounded-[6vh] border-2 border-black"
@@ -61,13 +140,31 @@ const AboutSection = () => {
 							className="w-[15vw] h-[13vh] object-cover rounded-[6vh] border-2 border-black"
 							alt=""
 						/>
-					</div>
-				</div>
-				<div className="w-[85vw] border-2 border-black rounded-[10vh] flex items-center justify-between min-h-[15vh] px-[3vw] py-[2vh]">
-					<div className="text-[3vh] pl-[1vw]">
+					</motion.div>
+				</motion.div>
+
+				{/* Card 3 */}
+				<motion.div
+					ref={ref3}
+					initial="hidden"
+					animate={controls3}
+					variants={fadeInUp(0.5)}
+					className="w-[85vw] border-2 border-black rounded-[10vh] flex items-center justify-between min-h-[15vh] px-[3vw] py-[2vh]"
+				>
+					<motion.div
+						initial="hidden"
+						animate={controls3}
+						variants={fadeInUp(0.7)}
+						className="text-[3vh] pl-[1vw]"
+					>
 						Lorem ipsum dolor sit amet consectetur adipisicing.
-					</div>
-					<div className="flex gap-[1vw]">
+					</motion.div>
+					<motion.div
+						initial="hidden"
+						animate={controls3}
+						variants={fadeInUp(0.9)}
+						className="flex gap-[1vw]"
+					>
 						<img
 							src="/banner.jpg"
 							className="w-[15vw] h-[13vh] object-cover rounded-[6vh] border-2 border-black"
@@ -78,8 +175,8 @@ const AboutSection = () => {
 							className="w-[15vw] h-[13vh] object-cover rounded-[6vh] border-2 border-black"
 							alt=""
 						/>
-					</div>
-				</div>
+					</motion.div>
+				</motion.div>
 			</div>
 		</div>
 	);
--- components/pages/landing/CardsSection.tsx
+++ components/pages/landing/CardsSection.tsx
@@ -1,9 +1,41 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { easeOut } from "framer-motion";
+
+const fadeInUp = (delay = 0) => ({
+	hidden: { opacity: 0, y: 30 },
+	visible: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			duration: 0.4,
+			delay,
+			ease: easeOut,
+		},
+	},
+});
 
 const CardsSection = () => {
+	const controls1 = useAnimation();
+	const controls2 = useAnimation();
+	const controls3 = useAnimation();
+
+	const [ref1, inView1] = useInView({ triggerOnce: true });
+	const [ref2, inView2] = useInView({ triggerOnce: true });
+	const [ref3, inView3] = useInView({ triggerOnce: true });
+
+	useEffect(() => {
+		if (inView1) controls1.start("visible");
+		if (inView2) controls2.start("visible");
+		if (inView3) controls3.start("visible");
+	}, [inView1, inView2, inView3, controls1, controls2, controls3]);
+
 	return (
 		<div className="">
 			<div className="h-[105vh] flex flex-col gap-[3vh] items-center justify-center w-[85vw] mx-auto relative">
+				{/* Star images (static) */}
 				<img
 					src="/stars/image 1.png"
 					className="absolute top-[6vh] right-[-5vw] w-[20vw]"
@@ -14,67 +46,140 @@ const CardsSection = () => {
 					className="absolute bottom-[4vh] z-[-1] left-[-5vw] w-[10vw]"
 					alt=""
 				/>
+
+				{/* Cards Row */}
 				<div className="flex gap-[2vw] w-full">
-					<div className="flex flex-col w-1/3 gap-[3vh]">
-						<div className="w-full h-[55vh] rounded-[12.5vh] overflow-hidden box-border border-2 border-black relative">
-							<div className="absolute top-[1vh] left-[50%] translate-x-[-50%] border-[1.5px] border-black py-[0.5vh] px-[2vw] bg-white rounded-full">
+					{/* Card 1 */}
+					<motion.div
+						ref={ref1}
+						initial="hidden"
+						animate={controls1}
+						variants={fadeInUp(0)}
+						className="flex flex-col w-1/3 gap-[3vh]"
+					>
+						<motion.div
+							initial="hidden"
+							animate={controls1}
+							variants={fadeInUp(0.2)}
+							className="w-full h-[55vh] rounded-[12.5vh] overflow-hidden box-border border-2 border-black relative"
+						>
+							<motion.div
+								initial="hidden"
+								animate={controls1}
+								variants={fadeInUp(0.4)}
+								className="absolute top-[1vh] left-[50%] translate-x-[-50%] border-[1.5px] border-black py-[0.5vh] px-[2vw] bg-white rounded-full"
+							>
 								<p className="font-light text-[2vh]">Lorem Ipsum</p>
-							</div>
-							<img
+							</motion.div>
+							<motion.img
 								src="/banner.jpg"
 								className="w-full h-full object-cover"
 								alt=""
 							/>
-						</div>
-						<div className="bg-neutral-100 rounded-[6.5vh] min-h-[15vh] w-full box-border border-2 border-black flex flex-col items-start justify-center px-[3vw]">
+						</motion.div>
+
+						<motion.div
+							initial="hidden"
+							animate={controls1}
+							variants={fadeInUp(0.8)}
+							className="bg-neutral-100 rounded-[6.5vh] min-h-[15vh] w-full box-border border-2 border-black flex flex-col items-start justify-center px-[3vw]"
+						>
 							<h2 className="text-[3vh] font-medium mb-[0.25vh]">
 								Lorem Ipsum
 							</h2>
 							<p className="text-[2.25vh] font-extralight">
 								Lorem ipsum dolor sit.
 							</p>
-						</div>
-					</div>
-					<div className="flex flex-col w-1/3 gap-[2vh]">
-						<div className="w-full h-[55vh] rounded-[12.5vh] overflow-hidden box-border border-2 border-black relative">
-							<div className="absolute top-[1vh] left-[50%] translate-x-[-50%] border-[1.5px] border-black py-[0.5vh] px-[2vw] bg-white rounded-full">
+						</motion.div>
+					</motion.div>
+
+					{/* Card 2 */}
+					<motion.div
+						ref={ref2}
+						initial="hidden"
+						animate={controls2}
+						variants={fadeInUp(0.1)}
+						className="flex flex-col w-1/3 gap-[2vh]"
+					>
+						<motion.div
+							initial="hidden"
+							animate={controls2}
+							variants={fadeInUp(0.3)}
+							className="w-full h-[55vh] rounded-[12.5vh] overflow-hidden box-border border-2 border-black relative"
+						>
+							<motion.div
+								initial="hidden"
+								animate={controls2}
+								variants={fadeInUp(0.5)}
+								className="absolute top-[1vh] left-[50%] translate-x-[-50%] border-[1.5px] border-black py-[0.5vh] px-[2vw] bg-white rounded-full"
+							>
 								<p className="font-light text-[2vh]">Lorem Ipsum</p>
-							</div>
-							<img
+							</motion.div>
+							<motion.img
 								src="/banner.jpg"
 								className="w-full h-full object-cover"
 								alt=""
 							/>
-						</div>
-						<div className="bg-neutral-100 rounded-[6.5vh] min-h-[15vh] w-full box-border border-2 border-black flex flex-col items-start justify-center px-[3vw]">
+						</motion.div>
+
+						<motion.div
+							initial="hidden"
+							animate={controls2}
+							variants={fadeInUp(0.9)}
+							className="bg-neutral-100 rounded-[6.5vh] min-h-[15vh] w-full box-border border-2 border-black flex flex-col items-start justify-center px-[3vw]"
+						>
 							<h2 className="text-[3vh] font-medium mb-[0.25vh]">
 								Lorem Ipsum
 							</h2>
 							<p className="text-[2.25vh] font-extralight">
 								Lorem ipsum dolor sit.
 							</p>
-						</div>
-					</div>
-					<div className="flex flex-col w-1/3 gap-[2vh]">
-						<div className="w-full h-[55vh] rounded-[12.5vh] overflow-hidden box-border border-2 border-black relative">
-							<div className="absolute top-[1vh] left-[50%] translate-x-[-50%] border-[1.5px] border-black py-[0.5vh] px-[2vw] bg-white rounded-full">
+						</motion.div>
+					</motion.div>
+
+					{/* Card 3 */}
+					<motion.div
+						ref={ref3}
+						initial="hidden"
+						animate={controls3}
+						variants={fadeInUp(0.2)}
+						className="flex flex-col w-1/3 gap-[2vh]"
+					>
+						<motion.div
+							initial="hidden"
+							animate={controls3}
+							variants={fadeInUp(0.4)}
+							className="w-full h-[55vh] rounded-[12.5vh] overflow-hidden box-border border-2 border-black relative"
+						>
+							<motion.div
+								initial="hidden"
+								animate={controls3}
+								variants={fadeInUp(0.6)}
+								className="absolute top-[1vh] left-[50%] translate-x-[-50%] border-[1.5px] border-black py-[0.5vh] px-[2vw] bg-white rounded-full"
+							>
 								<p className="font-light text-[2vh]">Lorem Ipsum</p>
-							</div>
-							<img
+							</motion.div>
+							<motion.img
 								src="/banner.jpg"
 								className="w-full h-full object-cover"
 								alt=""
 							/>
-						</div>
-						<div className="bg-neutral-100 rounded-[6.5vh] min-h-[15vh] w-full box-border border-2 border-black flex flex-col items-start justify-center px-[3vw]">
+						</motion.div>
+
+						<motion.div
+							initial="hidden"
+							animate={controls3}
+							variants={fadeInUp(1.0)}
+							className="bg-neutral-100 rounded-[6.5vh] min-h-[15vh] w-full box-border border-2 border-black flex flex-col items-start justify-center px-[3vw]"
+						>
 							<h2 className="text-[3vh] font-medium mb-[0.25vh]">
 								Lorem Ipsum
 							</h2>
 							<p className="text-[2.25vh] font-extralight">
 								Lorem ipsum dolor sit.
 							</p>
-						</div>
-					</div>
+						</motion.div>
+					</motion.div>
 				</div>
 			</div>
 		</div>
