04dba5985de29261319c0a30d21890d12107abd3
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2025-07-13 18:51:23 -0600

    login and register modals are done

---
+++ components/Inputs/SelectInput.tsx
@@ -0,0 +1,54 @@
+// components/Inputs/SelectInput.tsx
+"use client";
+
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface SelectInputProps {
+	id: string;
+	label: string;
+	options: string[];
+	disabled?: boolean;
+	required?: boolean;
+	register: UseFormRegister<FieldValues>;
+	errors: FieldErrors;
+}
+
+const SelectInput = ({
+	id,
+	label,
+	options,
+	disabled,
+	required,
+	register,
+	errors,
+}: SelectInputProps) => {
+	return (
+		<div className="w-full relative my-1">
+			<select
+				id={id}
+				disabled={disabled}
+				defaultValue={options[0]}
+				{...register(id, { required })}
+				className={`peer w-full p-3 pt-6 pl-4 font-light bg-neutral-300/75 border-2 border-neutral-400/75 text-black rounded-[2vh] outline-none transition disabled:opacity-70 disabled:cursor-not-allowed appearance-none
+          ${errors[id] ? "border-rose-400" : "border-neutral-300"}
+          ${errors[id] ? "focus:border-rose-500" : "focus:border-neutral-900"}
+        `}
+			>
+				{options.map((option) => (
+					<option key={option} value={option.toLowerCase()}>
+						{option}
+					</option>
+				))}
+			</select>
+			<label
+				className={`absolute text-md duration-150 transform scale-75 -translate-y-4 top-5 left-4 z-10 origin-[0]
+            ${errors[id] ? "text-rose-300 font-semibold" : "text-zinc-600"}
+        `}
+			>
+				{label}
+			</label>
+		</div>
+	);
+};
+
+export default SelectInput;
---
+++ components/modals/Modal.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import Button from "./Button";
+
+interface ModalProps {
+	isOpen?: boolean;
+	onClose: () => void;
+	onSubmit: () => void;
+	title?: string;
+	body?: React.ReactElement;
+	footer?: React.ReactElement;
+	actionLabel: string;
+	disabled: boolean;
+	secondaryAction?: () => void;
+	secondaryActionLabel?: string;
+}
+
+const Modal = ({
+	isOpen,
+	onClose,
+	onSubmit,
+	title,
+	body,
+	footer,
+	actionLabel,
+	disabled,
+	secondaryAction,
+	secondaryActionLabel,
+}: ModalProps) => {
+	const [showModal, setShowModal] = useState(isOpen);
+
+	useEffect(() => {
+		setShowModal(isOpen);
+	}, [isOpen]);
+
+	const handleClose = useCallback(() => {
+		if (disabled) return;
+
+		setShowModal(false);
+
+		setTimeout(() => {
+			onClose();
+		}, 300);
+	}, [disabled, onClose]);
+
+	const handleSubmit = useCallback(() => {
+		if (disabled) return;
+		onSubmit();
+	}, [disabled, onSubmit]);
+
+	const handleSecondaryAction = useCallback(() => {
+		if (disabled || !secondaryAction) return;
+
+		secondaryAction();
+	}, [disabled, secondaryAction]);
+
+	if (!isOpen) return null;
+
+	return (
+		<>
+			<div className="justify-center items-center flex overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70 backdrop-blur-md h-screen ">
+				<div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-screen lg:h-auto md:h-auto overflow-y-hidden">
+					{/* CONTENT */}
+					<div
+						className={`translate duration-300 h-full ${
+							showModal
+								? "translate-y-0 opacity-100"
+								: "translate-y-full opacity-0"
+						}`}
+					>
+						<div className="translate h-full lg:h-auto md:h-auto border-0 rounded-[10vh] relative flex flex-col w-full bg-white/[70%] outline-none focus:outline-none text-black">
+							{/* HEADER */}
+							<div className="flex items-center p-6 rounded-t justify-center relative border-b-[1.5px] border-neutral-800/50">
+								<button
+									className="p-2 text-black rounded-full hover:bg-neutral-400/50 cursor-pointer border-0 transition absolute right-[4vw]"
+									onClick={handleClose}
+								>
+									<IoMdClose size={24} />
+								</button>
+								<div className="text-xl text-black maestro">{title}</div>
+							</div>
+							{/* BODY */}
+							<div className="relative p-6 py-4 flex-auto">{body}</div>
+							{/* FOOTER */}
+							<div className="flex flex-col gap-2 p-6 py-4 w-full">
+								<div className="flex flex-row items-center gap-4 w-full">
+									{secondaryAction && secondaryActionLabel && (
+										<Button
+											disabled={disabled}
+											label={secondaryActionLabel}
+											onClick={handleSecondaryAction}
+											outline
+										/>
+									)}
+									<Button
+										disabled={disabled}
+										label={actionLabel}
+										onClick={handleSubmit}
+									/>
+								</div>
+								{footer}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Modal;
