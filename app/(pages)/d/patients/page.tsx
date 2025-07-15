bd9ad3820a18ec596b9adb8655d91e1f80e41233
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2025-07-15 11:19:58 -0600

    patients page

---
+++ app/(pages)/d/patients/page.tsx
@@ -0,0 +1,26 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import Heading from "@/components/pages/dashboard/Heading";
+import DoctorNavbar from "@/components/pages/dashboard/doctorDashboard/PatientNavbar";
+import PatientsPage from "@/components/pages/doctorPatients/PatientsPage";
+import React from "react";
+
+const Page = async () => {
+	const currentUser = await getCurrentUser();
+
+	if (!currentUser) {
+		return <div>Please log in to view this page.</div>;
+	}
+
+	return (
+		<div className="flex">
+			<DoctorNavbar />
+
+			<div className="w-[93.5vw] flex flex-col py-[5vh] px-[4vw]">
+				<Heading />
+				<PatientsPage currentUser={currentUser} />
+			</div>
+		</div>
+	);
+};
+
+export default Page;
